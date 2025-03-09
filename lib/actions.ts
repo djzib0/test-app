eb658244d3d70b1513540f15a3885741c625a4ad
djzib0 <piotrsabatura@gmail.com>
2025-03-09 12:38:35 +0100

    test project initialization

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,7 @@
 import { Entry } from "@/lib/models";
-import { connectToDb } from "@/lib/utils";
+import { connectToDb } from "@/lib/utils"
 import { NextRequest, NextResponse } from "next/server";
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const GET = async (request: NextRequest) => {
---
+++ lib/utils.ts
@@ -0,0 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { default: mongoose } = require("mongoose")  
+
+export const connectToDb = async () => {
+    const connection: {isConnected: boolean | undefined} = {
+    isConnected: undefined
+};
+
+    try {
+        if(connection.isConnected) {  
+            console.log("Using existing connection")
+            return;
+        }
+        const db = await mongoose.connect(process.env.MONGO); 
+        connection.isConnected = db.connection[0].readyState;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        console.log(error)
+        throw new Error(error);
+    }
+}
+
+export const getEntriesData = async () => {
+    const res = await fetch(`${process.env.API_URL}/api/entries`)
+  
+    if (!res.ok) {
+      throw new Error("Something went wrong")
+    }
+  
+    return res.json();
+  }
+
+  export const capitalizeFirstLetter = (str: string): string =>  {
+    return str.slice(0, 1).toUpperCase() + str.slice(1)
+  }
