603914aa51d8730c447a187ff0c945515043999b
djzib0 <piotrsabatura@gmail.com>
2025-03-09 13:07:11 +0100

    removed fetching

--- app/(root)/entries/page.tsx
+++ app/(root)/entries/page.tsx
@@ -1,27 +1,12 @@
-import EntriesContainer from '@/app/components/entriesContainer/EntriesContainer';
-import { getEntriesData } from '@/lib/utils';
+
 import React from 'react'
 
 const EntriesPage = async () => {
 
-  const entries = await getEntriesData();
-
-  if (!entries) {
-    return (
-      <div>No data</div>
-    )
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const entriesArr = entries.map((entry: any) => {
-    return (
-      <p key={entry._id}>{entry.title}</p>
-    )
-  })
 
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-        <EntriesContainer entries={entriesArr} />
+        <p>For now there is no data</p>
     </div>
   )
 }
